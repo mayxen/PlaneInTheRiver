a80219a2df8889fdb269a499dae195686188b1fc
ivan Curbelo <ivancurbelotrabajo@gmail.com>
2018-10-29 18:33:03 +0000

    shoot
    el avion dispara

--- Assets/proyect/Scripts 1/PlayerController.cs
+++ Assets/proyect/Scripts 1/PlayerController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    public float horSpeed = 5;
-    public float verSpeed = 5;
+    public float horSpeed = 5f;
+    public float verSpeed = 5f;
+    public float missileSpeed = 10f;
     public float horizontalLimit;
     public float verticalLimit;
+    public GameObject missile;
+    bool isFiring;
     // Use this for initialization
     void Start () {
 		horizontalLimit = Camera.main.pixelWidth / 2 * Camera.main.aspect / 100 -2;
@@ -27,6 +30,23 @@ public class PlayerController : MonoBehaviour {
         else if (transform.position.x < -horizontalLimit)
         {
             transform.position = new Vector2(-horizontalLimit, transform.position.y);
+        }
+
+        if (Input.GetAxis("Fire1")== 1f)//de esta forma tiene que soltar el fire1 para poder volver a disparar
+        {
+            if (!isFiring)
+            {
+                isFiring = true;
+                GameObject fire = Instantiate(missile);
+                fire.transform.SetParent(transform.parent);
+                fire.transform.position = transform.position;
+                fire.GetComponent<Rigidbody2D>().velocity = Vector2.up * missileSpeed;
+                Destroy(fire, 4f);
+            }
+            
+        }else
+        {
+            isFiring = false;
         }
         
     }
